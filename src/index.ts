451fc9111e26c4314c44f7c95f33df3948b7108a
Sean Behan <codebam@riseup.net>
2023-12-03 12:35:32 -0500

    use links for diff instead of the entire item

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,18 @@ export default {
 				`https://api.telegram.org/bot${env.TELEGRAM_TOKEN}/sendMessage?` + new URLSearchParams({ chat_id: '@canadanewschannel', text })
 			);
 		const rss = ['https://www.cbc.ca/webfeed/rss/rss-topstories', 'http://ctvnews.ca/rss/TopStories'];
-		Promise.all(
+		await Promise.all(
 			rss.map(async (url) => {
 				const response = await fetch(url);
 				const text = await response.text();
 				const feed = htmlparser2.parseFeed(text);
 				const last = feed?.items[0] ?? { link: '' };
-				const last_str = JSON.stringify(last);
 				const last_kv = await env.KV.get(url);
-				if (last_str !== last_kv) {
+				if (last.link !== last_kv) {
 					const send_message = telegram_message(last.link ?? '');
 					const response = await fetch(send_message);
 					console.log(await response.text());
-					await env.KV.put(url, last_str);
+					await env.KV.put(url, last.link ?? '');
 				}
 			})
 		);
