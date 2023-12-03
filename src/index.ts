efac3077123290c8d68f8f6e6dbfa80f54377a93
Sean Behan <codebam@riseup.net>
2023-12-02 23:27:29 -0500

    rss bot

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as htmlparser2 from 'htmlparser2';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	KV: KVNamespace;
+	TELEGRAM_TOKEN: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	scheduled: async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => {
+		const telegram_message = (text: string) =>
+			new URL(
+				`https://api.telegram.org/bot${env.TELEGRAM_TOKEN}/sendMessage?` + new URLSearchParams({ chat_id: '@canadanewschannel', text })
+			);
+		const rss = ['https://www.cbc.ca/webfeed/rss/rss-canada', 'http://ctvnews.ca/rss/TopStories'];
+		for (const url of rss) {
+			const response = await fetch(url);
+			const text = await response.text();
+			const feed = htmlparser2.parseFeed(text);
+			const last = feed?.items[0] ?? { link: '' };
+			const last_str = JSON.stringify(last);
+			const last_kv = await env.KV.get(url);
+			if (last_str !== last_kv) {
+				const send_message = telegram_message(last.link ?? '');
+				const response = await fetch(send_message);
+				console.log(await response.text());
+				await env.KV.put(url, last_str);
+			}
+		}
+		return new Response('ok');
 	},
 };
